276ed106325fcc4f8c47d3b4ecb38e37e2d7fe85
Abdellah <abdallahkaba98@gmail.com>
2024-03-10 18:00:38 +0000

    Input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {Input} from "antd"
+import {UserOutlined} from "@ant-design/icons";
+
+
 function App() {
-  return (
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <header className="App-header">
+           <Input.Search
+               placeholder='Name'
+               prefix={<UserOutlined />}
+               maxLength={5}
+               allowClear
+
+
+           >
+
+           </Input.Search>
+
+       </header>
+
     </div>
   );
 }
