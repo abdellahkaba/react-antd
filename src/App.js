6bcffe5330adbd72d63d6878d6a7de51d5c1251a
Abdellah <abdallahkaba98@gmail.com>
2024-03-11 07:05:24 +0000

    Table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import './App.css';
-import {Form, Button, Input} from "antd";
-
-
-function App() {
-    const onFinish = (e) => {
-        console.log(e)
+import {Table} from "antd";
+const data = [
+    {
+        name : "Kaba",
+        age : 25,
+        adress : "Poste Thiaroye",
+        key : 1
+    },
+    {
+        name : "N'diaye",
+        age : 28,
+        adress : "Dakar",
+        key : 2
+    },
+    {
+        name : "Balla",
+        age : 26,
+        adress : "Pikin",
+        key : 3
+    },
+    {
+        name : "Kamara",
+        age : 18,
+        adress : "Medine",
+        key : 4
     }
+]
+const columns = [
+    {
+        title: 'Name',
+        dataIndex:'name',
+        key: 'key',
+        render: name => {
+            return <a>{name}</a>
+        }
+    },
+    {
+        title: 'Age',
+        dataIndex:'age',
+        key: 'key',
+        sorter: (a,b) => a.age - b.age
+    }, {
+        title: 'Adress',
+        dataIndex:'adress',
+        key: 'key'
+    },
+    {
+        title: 'Graduated?',
+        key: 'key',
+        render: payload => {
+          return <p>{payload.age>20?'True':'False'}</p>
+        }
+    }
+]
+function App() {
     return (
     <div className="App">
        <header className="App-header">
-         <Form onFinish={onFinish}>
-             <Form.Item label="User Name" name="username">
-                 <Input placeholder="User name" required></Input>
-             </Form.Item>
-             <Form.Item label="Mot de pass" name="password">
-                 <Input.Password placeholder="Mot de passe"></Input.Password>
-             </Form.Item>
-             <Form.Item>
-                 <Button block type='primary' htmlType='submit'>Login</Button>
-             </Form.Item>
-         </Form>
+            <Table
+                dataSource={data}
+                columns={columns}
+                pagination
+            >
+
+            </Table>
        </header>
 
     </div>
