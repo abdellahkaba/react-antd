f55b8274f55ea0a32906f32a0c766e47fd267609
Abdellah <abdallahkaba98@gmail.com>
2024-03-13 02:33:54 +0000

    Message

--- src/App.js
+++ src/App.js
@@ -1,77 +1,34 @@
 import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
-const onFinish = (values) => {
-    console.log('Success:', values);
-};
-const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-};
-const App = () => (
-    <Form
-        name="basic"
-        labelCol={{
-            span: 8,
-        }}
-        wrapperCol={{
-            span: 16,
-        }}
-        style={{
-            maxWidth: 600,
-        }}
-        initialValues={{
-            remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-    >
-        <Form.Item
-            label="Username"
-            name="username"
-            rules={[
-                {
-                    required: true,
-                    message: 'Please input your username!',
-                },
-            ]}
-        >
-            <Input />
-        </Form.Item>
+import {Button, Form, Input, message, Table} from 'antd';
+import "./App.css"
 
-        <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-                {
-                    required: true,
-                    message: 'Please input your password!',
-                },
-            ]}
-        >
-            <Input.Password />
-        </Form.Item>
 
-        <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{
-                offset: 8,
-                span: 16,
-            }}
-        >
-            <Checkbox>Remember me</Checkbox>
-        </Form.Item>
 
-        <Form.Item
-            wrapperCol={{
-                offset: 8,
-                span: 16,
-            }}
-        >
-            <Button type="primary" htmlType="submit">
-                Submit
-            </Button>
-        </Form.Item>
-    </Form>
-);
+const App = () => {
+    const onFinish = (e) => {
+        console.log(e)
+        setTimeout(() => {
+            message.success("Login success")
+            // message.error("error desole !")
+            //message.warning("Avertissement")
+        },2000)
+    }
+    return (
+        <div className="App">
+            <header className="container-fluid bg-info text-white min-vh-100 d-flex flex-column align-items-center justify-content-center">
+                <Form onFinish={onFinish}>
+                    <Form.Item  label="UserName" name="username">
+                        <Input  placeholder="User name" required></Input>
+                    </Form.Item>
+                    <Form.Item label="Mot de pass" name="password">
+                        <Input.Password placeholder="Mot de passe"></Input.Password>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button block type='primary' htmlType='submit'>Login</Button>
+                    </Form.Item>
+                </Form>
+            </header>
+        </div>
+
+)};
 export default App;
